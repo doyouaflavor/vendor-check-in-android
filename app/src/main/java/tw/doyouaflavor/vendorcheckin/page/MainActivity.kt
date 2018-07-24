fc42858e6182a38965132cc084564ba7c4dacce9
Jarvis Lin <jarvislin0202@gmail.com>
2018-07-24 11:24:27 +0800

    add files

---
+++ app/src/main/java/tw/doyouaflavor/vendorcheckin/page/MainActivity.kt
@@ -0,0 +1,13 @@
+package tw.doyouaflavor.vendorcheckin.page
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import tw.doyouaflavor.vendorcheckin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
